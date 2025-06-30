5a0948947c293cee190a25db888dfdfbd5170b7e
niccolosea <niccolosea@gmail.com>
2025-06-30 11:49:54 +0400

    Adding exercise N4

---
+++ Mad-Lib-#4/main.py
@@ -0,0 +1,8 @@
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+adjective = input("Enter a adjective: ")
+adverb = input("Enter a adverb: ")
+
+answer = input(f"Do you like {verb} {noun}?")
+
+print(f"Woww, I read very {adjective} books and very {adverb}.")
