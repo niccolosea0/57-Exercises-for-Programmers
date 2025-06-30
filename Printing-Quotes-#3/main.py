0789f473f86c60e2782541d9148ea9c249a0f1c1
niccolosea <niccolosea@gmail.com>
2025-06-30 11:49:12 +0400

    Adding third exercise

---
+++ Printing-Quotes-#3/main.py
@@ -0,0 +1,11 @@
+output = ("Lorem Ipsum is simply dummy text "
+          "of the printing and typesetting industry.\n"
+          "\"Lorem Ipsum has been the industry's\" standard dummy\n"
+          "text ever \"since the 1500s\", when an\n"
+          "unknown 'printer took a galley' of type and\n"
+          "scrambled it to make a type specimen book.")
+
+print(output)
+
+# Escape characters \" (text) \"
+
