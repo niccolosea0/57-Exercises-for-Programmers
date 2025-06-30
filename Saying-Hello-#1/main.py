76c23c6474d63f6e6146475c8018cd37828cd1e4
niccolosea <niccolosea@gmail.com>
2025-06-30 11:47:29 +0400

    Adding first challange sayinh hello

---
+++ Saying-Hello-#1/main.py
@@ -0,0 +1,13 @@
+name = input("What is your name? ")
+print(f"Hello, {name} nice to meet you")
+
+# --- Without using variables
+# print(f"Hello {input("What is your name? ")} it is very nice to meet you.")
+
+# --- Challenge 2: Different outputs for different people
+# if name == "Nikoloz":
+#     print("Wow our 2nd computer science student.")
+# elif name == "John Carmack":
+#     print("Let's stand up for legend of first-person shooter game.")
+# else:
+#     print("Welcome Welcome, please come in.")
