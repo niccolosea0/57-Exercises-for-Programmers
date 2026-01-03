8ec8cffc751f41602ada118294ae3bfb8c323c81
niccolosea0 <niccolosea@gmail.com>
2026-01-03 06:30:47 +0400

    Add SimpleMath#5 exercise

---
+++ Simple-Math-#5/SimpleMath.java
@@ -0,0 +1,52 @@
+/*
+ *   File: SimpleMath.java
+ *   
+ *   You’ll often write programs that deal with numbers. And depending on the programming language you use, you’ll
+ *   have to convert the inputs you get to numerical data types. Write a program that prompts for two numbers. Print the
+ *   sum, difference, product, and quotient of those numbers as shown in the example output:
+ *   
+ *   Example Output
+ *   What is the first number? 10
+ *   What is the second number? 5
+ *   10 + 5 = 15
+ *   10 - 5 = 5
+ *   10 * 5 = 50
+ *   10 / 5 = 2
+ *   
+ *   Constraints
+ *   • Values coming from users will be strings. Ensure that you convert these values to numbers before doing the math.
+ *   • Keep the inputs and outputs separate from the numerical conversions and other processing.
+ *   • Generate a single output statement with line breaks in the appropriate spots.
+ *   
+ *   Challenges
+ *   • Revise the program to ensure that inputs are entered as
+ *     numeric values. Don’t allow the user to proceed if the value entered is not numeric.
+ *   • Don’t allow the user to enter a negative number.
+ *   • Break the program into functions that do the computations. You’ll explore functions in Chapter 5, Functions, on page 45.
+ *   • Implement this program as a GUI program that automat- ically updates the values when any value changes.
+ */
+ 
+ import java.util.Scanner;
+
+ public class SimpleMath {
+     public static void main(String[] args) {
+
+         Scanner scanner = new Scanner(System.in);
+
+         System.out.print("What is the first number? ");
+         String firstNumber = scanner.nextLine();
+
+         System.out.print("What is the second number? ");
+         String secondNumber = scanner.nextLine();
+
+         int firstNum = Integer.parseInt(firstNumber); 
+         int secondNum = Integer.parseInt(secondNumber);
+
+         System.out.println(firstNum + " + " + secondNum + " = " + (firstNum + secondNum));
+         System.out.println(firstNum + " - " + secondNum + " = " + (firstNum - secondNum));
+         System.out.println(firstNum + " * " + secondNum + " = " + (firstNum * secondNum));
+         System.out.println(firstNum + " / " + secondNum + " = " + (firstNum / secondNum));
+
+
+     }
+}
