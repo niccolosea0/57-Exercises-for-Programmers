f145a2b44299bff09e17a57f8a471dc9637f4336
niccolosea0 <niccolosea@gmail.com>
2026-01-11 14:23:02 +0400

    Add support for L-shaped rooms

--- 09_paint_calculator/PaintCalculator.java
+++ 09_paint_calculator/PaintCalculator.java
@@ -36,6 +36,8 @@ public class PaintCalculator {
     public static void main(String[] args) {
         
         /*
+        // ORIGINAL TASK, NO CHALLANGES
+
         int length = getNumber("What is length of the room? ");
 
         int width = getNumber("What is width of the room? ");
@@ -47,7 +49,9 @@ public class PaintCalculator {
         System.out.println("You will need to purchase " + (int) gallonsNeededRounded + " gallons of paint to cover " + roomArea + " square feet.");
         */
 
-        // Implement support for a round room
+        /*
+        // IMPLEMENT SUPPORT FOR A ROUND ROOM
+
         int radius  = getNumber("What is the radius of the round room? ");
 
         // Area of the round room
@@ -57,7 +61,33 @@ public class PaintCalculator {
         double gallonsNeeded = roundRoomArea / GALLON; 
        
         System.out.println("You will need to purchase " + (int) Math.ceil(gallonsNeeded) + " gallons of paint to cover " + Math.round(roundRoomArea) + " square feet.");
-         
+        */
+
+        // IMPLEMENT SUPPORT FOR AN L-SHAPED ROOM
+
+        /*In order to calculate area of the L-shaped room we need to divide it in 2 rectangles, like left and right rectangle
+          In this program we will have left and right rectangles and their corresponding sizes, then we will calculate area of entire rectangle
+        */
+
+        // Left rectangle
+        int leftRectLength = getNumber("Enter length of the left rectangle: ");
+        int leftRectWidth = getNumber("Enter width of the left rectangle: ");
+
+        // Right rectangle
+        int rightRectLength = getNumber("Enter length of the right rectangle: ");
+        int rightRectWidth = getNumber("Enter width of the right rectangle: ");
+
+        // Calculate area os rectangles
+        int leftRectArea = leftRectLength * leftRectWidth;
+
+        int rightRectArea = rightRectLength * rightRectWidth;
+
+        // Area of entire rectangle
+        int totalArea = leftRectArea + rightRectArea;
+        double gallonsNeeded = (double) totalArea / GALLON;
+
+        System.out.println("\nYou will need to purchase " + (int) Math.ceil(gallonsNeeded) + " gallons of paint to cover " + totalArea + " square feet.");
+
     }
 
     private static int getNumber(String prompt) {
