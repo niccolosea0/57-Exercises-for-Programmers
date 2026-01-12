c672919007a0556222c9a387f3dc0656a1a4f2c1
niccolosea0 <niccolosea@gmail.com>
2026-01-12 16:05:24 +0400

    Add challange2 of Exercise10

--- 10_self_checkout/SelfCheckout.java
+++ 10_self_checkout/SelfCheckout.java
@@ -7,35 +7,14 @@ public class SelfCheckout {
 
     public static void main(String[] args) {
         
-        // First item
-        System.out.println();
-
-        int firstItemPrice = getNumber("Enter price of the item 1: ");
-        int firstItemQuantity = getNumber("Enter quantity of the item 1: ");
-
-
-        // Second item
-        System.out.println();
-
-        int secondItemPrice = getNumber("Enter price of the item 2: ");
-        int secondItemQuantity = getNumber("Enter quantity of the item 2: ");
-
-
-        // Third item
-        System.out.println();
-
-        int thirdItemPrice = getNumber("Enter price of the item 3: ");
-        int thirdItemQuantity = getNumber("Enter quantity of the item 3: ");
-
-        // Calculate subtotal, tax, total
-        double subTotal = (firstItemPrice * firstItemQuantity) + (secondItemPrice * secondItemQuantity) + (thirdItemPrice * thirdItemQuantity);
+        // Get subtotal and calculate tax and total
+        double subTotal = calculateSubTotal();
         double tax = subTotal * 5.5 / 100;
         double total = subTotal + tax;
 
         System.out.printf("\nSubtotal: $%.2f\n", subTotal);
         System.out.printf("Tax: $%.2f\n", tax);
         System.out.printf("Total: $%.2f\n", total);
-
 
     }
 
@@ -48,6 +27,37 @@ public class SelfCheckout {
         }
 
         return scanner.nextInt();
+    }
+
+    private static double calculateSubTotal() {
+        System.out.println(); // New line for clarity
+        int itemsNumber = getNumber("How many items would you like to purchase? ");
+
+        double subTotal = 0;
+
+        for (int i = 1; i <= itemsNumber; i++) {
+
+            // Get particular item's price and quantity
+            int itemPrice = getItemPrice(i);
+            int itemQuantity = getItemQuantity(i);
+            
+            // Calculate cost of entire product (item * quantity)
+            int itemCost = itemPrice * itemQuantity;
+
+            // Add itemcost to subtotal
+            subTotal += itemCost;
+        }
+
+        return subTotal;
+    }
+
+    private static int getItemPrice(int index) {
+        System.out.println(); // Print new line for clarity
+        return getNumber("Enter price of the item " + index + ": ");
+    }
+
+    private static int getItemQuantity(int index) {
+        return getNumber("Enter quantity of the item " + index + ": ");
     }
 
 }
