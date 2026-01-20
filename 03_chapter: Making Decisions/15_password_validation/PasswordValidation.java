634221827c37b9792637d81e7dbfe122f380db1f
niccolosea0 <niccolosea@gmail.com>
2026-01-20 20:48:45 +0400

    Add challange1

--- 03_chapter: Making Decisions/15_password_validation/PasswordValidation.java
+++ 03_chapter: Making Decisions/15_password_validation/PasswordValidation.java
@@ -1,23 +1,33 @@
 /* File PasswordValidation.java */
 /* Simple programm that validates user's password */
 
-import java.util.Scanner;
+import java.io.Console;
+import java.util.Arrays;
 
 public class PasswordValidation {
     
-    private static Scanner scanner = new Scanner(System.in);
     private static final String myPassword = "Winnypuhhy%152";
 
     public static void main(String[] args) {
          
-         System.out.print("What is the password? ");
-         String userPassword = scanner.nextLine();
+         Console console = System.console();
 
-         if (userPassword.equals(myPassword)) {
+         if (console == null) {
+             System.out.println("No console available!");
+             return;
+         }
+
+         char[] password = console.readPassword("What is the password?");
+
+         String passwordStr = new String(password);
+
+         if (passwordStr.equals(myPassword)) {
              System.out.println("Welcome!");
          } else {
              System.out.println("I don't know you");
          }
 
+        // Clear password from memory
+        Arrays.fill(password, ' ');
     }
 }
