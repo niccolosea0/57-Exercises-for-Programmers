0b0a058529a3841a3864842b212c2cc0afb1a2a9
niccolosea0 <niccolosea@gmail.com>
2026-01-27 11:44:16 +0400

    Add methods and switch statement

--- 18_temperature_converter/TemperatureConverter.java
+++ 18_temperature_converter/TemperatureConverter.java
@@ -11,15 +11,30 @@ public class TemperatureConverter {
         System.out.println("Precc F to convert from Celsius to Fahrenheit");
         String choice = getChoice("Your choice: ");
 
-        if (choice.equals("c")) {
-            int temp = getNumber("Please enter the temperature in Fahrenheit: ");
-            System.out.println("The temperature in Celsius is " + toCelsius(temp));
-        } else if (choice.equals("f")) {
-            int temp = getNumber("Please enter the temperature in Celsius: ");
-            System.out.println("The temperature in Fahrenheit is " + toFahrenheit(temp));
+
+        switch (choice) {
+            case "c": 
+                calculateCelsius();
+                break;
+            case "f": 
+                calculateFahrenheit();
+                break;
+            default: 
+                System.out.println("How did you came here? get out");
+                break;
         }
-         
     }
+
+    private static void calculateCelsius() {
+        int temp = getNumber("Please enter the temperature in Fahrenheit: ");
+        System.out.println("The temperature in Celsius is " + toCelsius(temp));
+    }
+
+    private static void calculateFahrenheit() {
+        int temp = getNumber("Please enter the temperature in Celsius: ");
+        System.out.println("The temperature in Fahrenheit is " + toFahrenheit(temp));
+    }
+
 
     private static int toCelsius(int f) {
         double temp = (f - 32) * (5/9.);
