8b17dde3133bf491d70c83008c38bfd2536e96b6
niccolosea0 <niccolosea@gmail.com>
2026-01-31 14:34:07 +0400

    Modified challange1

--- 21_numbers_to_names/Solution.java
+++ 21_numbers_to_names/Solution.java
@@ -11,28 +11,30 @@ public class Solution {
         int number = getNumber("Please enter number of the month: ");
 
         Map<Integer, String> monthMap = new HashMap<>();
-        monthMap.put(1, "January");
-        monthMap.put(2, "February");
-        monthMap.put(3, "March");
-        monthMap.put(4, "April");
-        monthMap.put(5, "May");
-        monthMap.put(6, "June");
-        monthMap.put(7, "July");
-        monthMap.put(8, "August");
-        monthMap.put(9, "September");
-        monthMap.put(10, "Octomber");
-        monthMap.put(11, "November");
-        monthMap.put(12, "December");
 
-        String month = "";
-        if (monthMap.containsKey(number)) {
-            month = monthMap.get(number);
-        } else {
-            System.out.println("You have entered wrong number of month");
-            return;
+        putMonths(monthMap);
+
+        while (!monthMap.containsKey(number)) {
+            number = getNumber("Please enter number of the month between 1 and 12: ");
         }
 
+        String month = monthMap.get(number);
         System.out.println("The name of the month is " + month);
+    }
+
+    private static void putMonths(Map<Integer, String> map) {
+        map.put(1, "January");
+        map.put(2, "February");
+        map.put(3, "March");
+        map.put(4, "April");
+        map.put(5, "May");
+        map.put(6, "June");
+        map.put(7, "July");
+        map.put(8, "August");
+        map.put(9, "September");
+        map.put(10, "Octomber");
+        map.put(11, "November");
+        map.put(12, "December");
     }
 
     private static int getNumber(String prompt) {
