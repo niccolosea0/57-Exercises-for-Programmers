ec2a8198ac40712029505a2fcb9a0dbb84326a7d
niccolosea0 <niccolosea@gmail.com>
2026-02-09 11:26:20 +0400

    Add AddingNumbers task challange

--- 28_adding_numbers/AddingNumbers.java
+++ 28_adding_numbers/AddingNumbers.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
 
-public class AddinNumbers {
+public class AddingNumbers {
 
     public static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
+        int N = getNumber("How many numbers to add? ");
+
         int sum = 0;
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < N; i++) {
             int number = getNumber("Enter a number: ");
             sum += number;
         }
@@ -20,8 +22,8 @@ public class AddinNumbers {
     public static int getNumber(String prompt) {
         System.out.print(prompt);
         while (!scanner.hasNextInt()) {
-            System.out.println("Please enter valid integer!");
-            scanner.next();
+             System.out.println("Please enter valid integer!");
+             scanner.next();
         }
 
         return scanner.nextInt();
