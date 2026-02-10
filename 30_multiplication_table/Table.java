c93768a531dd0f470c2972137deb8020e7bcade0
niccolosea0 <niccolosea@gmail.com>
2026-02-10 13:41:00 +0400

    Add some part of challange2 for exercise30

--- 30_multiplication_table/Table.java
+++ 30_multiplication_table/Table.java
@@ -4,11 +4,17 @@ public class Table {
     
     public static void main(String[] args) {
 
+        int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        for (int elem : array) {
+            System.out.printf(elem + " ");
+        }
+        System.out.println();
         for (int i = 0; i <= N; i++) {
-            for ( int j = 0; j <= N; j++) {
-                System.out.printf("%d x %d = %d\n", i, j, (i * j));
+            System.out.printf(i + " ");
+            for (int j = 0; j <= N; j++) {
+                System.out.printf(i * array[j] + " ");
             }
-
+            System.out.println();
         }
         
          
