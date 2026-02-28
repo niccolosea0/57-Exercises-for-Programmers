147bb067c034e1cd708bd85a79f43af1e9c9d222
niccolosea0 <niccolosea@gmail.com>
2026-02-28 12:07:47 +0400

    Add EmployeeList task

--- 34_employee_list_removal/EmployeeList.java
+++ 34_employee_list_removal/EmployeeList.java
@@ -3,6 +3,9 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class EmployeeList {
+
+    public static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
         // Get list
@@ -12,17 +15,22 @@ public class EmployeeList {
         int employeeNum = list.size();
 
         // Print number of employeees
-        System.out.println("There are " + employeeNum + " employees:");
+        System.out.println("\nThere are " + employeeNum + " employees:");
 
         // Print employee's names
         printEmployeeList(list);
 
+        // Call remove employee method
+        removeEmployee(list);        
 
-        // Enter employe name which we want to remove
+        // Update employee's number
+        employeeNum = list.size();
 
-        // Print remaining employees
-        
-         
+        // Print number of employees
+        System.out.println("\nThere are " + employeeNum + " employees:");
+
+        // Print employees
+        printEmployeeList(list);
     }
 
     // Method that creates a Employee List and returns it
@@ -38,9 +46,35 @@ public class EmployeeList {
         return employeeList;
     }
 
+    // Method that prints the elements of the list
     public static void printEmployeeList(List<String> employeeList) {
         for (String name : employeeList) {
             System.out.println("\t" + name);
         }
+    }
+
+    public static void removeEmployee(List<String> employeeList) {
+        // Get employee name, needs to be removed
+        System.out.print("\nEnter an employee name to remove: ");
+        String employee = scanner.nextLine().trim();
+
+        if (checkEmployee(employeeList, employee)) {
+            int index = employeeList.indexOf(employee);
+            employeeList.remove(index);
+        } else {
+            System.out.println("There is not employee with that name, please specify valid name");
+            System.exit(1);
+        }
+    }
+
+    // Method to check if employee exists in the list
+    public static boolean checkEmployee(List<String> employeeList, String employeeName) {
+        for (String elem : employeeList) {
+            if (elem.equals(employeeName)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
