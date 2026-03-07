51a0ca7ed4d8b624ab9b83b0bc3289b7ec3eda9a
niccolosea0 <niccolosea@gmail.com>
2026-03-07 18:53:20 +0400

    Add task

--- 36_computing_statistics/ComputeStatistics.java
+++ 36_computing_statistics/ComputeStatistics.java
@@ -10,10 +10,18 @@ public class ComputeStatistics {
 
         List<Integer> list = new ArrayList<>();
         addNumbersInList(list);
-        System.out.println("Numbers:");
-        for (Integer elem : list) {
-            System.out.println(elem);
-        }
+        
+        double standDev = standardDeviation(list);
+        double mean = mean(list);
+        int max = max(list);
+        int min = min(list);
+
+
+        System.out.printf("Standard Deviation: %.2f\n", standDev);
+        System.out.println("Mean: " + mean);
+        System.out.println("Max: " + max);
+        System.out.println("Min: " + min);
+
         
     }
 
@@ -21,7 +29,7 @@ public class ComputeStatistics {
         while (true) {
             System.out.print("Enter a number: ");
             String number = scanner.nextLine();
-            if (number.equals("done") || number.equals("Done") {
+            if (number.equals("done") || number.equals("Done")) {
                 break;
             }
             try {
@@ -31,4 +39,68 @@ public class ComputeStatistics {
             }
         }
     }
+
+    private static double standardDeviation(List<Integer> numList) {
+
+        int size = numList.size();
+        double mean = mean(numList);
+        double[] array = new double[size];
+
+        // Step 1 -- Substract
+        for (int i = 0; i < size; i++) {
+            array[i] = Math.pow((numList.get(i) - mean), 2);
+        }
+
+        // Step 2 -- calculate mean
+        double sum = 0;
+        for (double elem : array) {
+            sum += elem;
+        }
+
+        double squaredMean = sum / size;
+
+        // Step3 -- take the square root
+        return Math.sqrt(squaredMean);
+    }
+
+    private static double mean(List<Integer> numList) {
+
+        int sum = 0;
+
+        int size = numList.size();
+
+        for (int i = 0; i < size; i++) {
+            sum += numList.get(i);
+        }
+
+        double mean = (double) sum / size;
+
+        return mean;
+    }
+
+    private static int min(List<Integer> numList) {
+
+        int min = numList.get(0);
+        for (int i = 1; i < numList.size(); i++) {
+            if (numList.get(i) < min) {
+                min = numList.get(i);
+            }
+        }
+
+        return min;
+    }
+
+    private static int max(List<Integer> numList) {
+
+        int max = numList.get(0);
+
+        for (int i = 1; i < numList.size(); i++) {
+            if (numList.get(i) > max) {
+                max = numList.get(i);
+            }
+        }
+
+        return max;
+    }
+
 }
