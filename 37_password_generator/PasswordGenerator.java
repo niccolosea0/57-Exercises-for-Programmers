b32d2afd4a24e4fdd8c62c8d07881643da3218a9
niccolosea0 <niccolosea@gmail.com>
2026-03-14 22:13:54 +0400

    Add getrandomchar

--- 37_password_generator/PasswordGenerator.java
+++ 37_password_generator/PasswordGenerator.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class PasswordGenerator {
 
@@ -6,13 +7,33 @@ public class PasswordGenerator {
 
     public static void main(String[] args) {
 
-        int passLength = getNumber("What's the minimum length? ");
-        int specialCount = getNumber("How mant special characters? ");
-        int numCount = getNumber("How many numbers? ");
+        Random rand = new Random();
+
+        // int passLength = getNumber("What's the minimum length? ");
+        // int specialCount = getNumber("How mant special characters? ");
+        // int numCount = getNumber("How many numbers? ");
+
+        char character = getRandomChar(rand);
+        System.out.print(character);
+
 
 
 
          
+    }
+
+    private static char getRandomChar(Random random) {
+
+        int randomNum = getRandomNumber(random, 97, 122);
+
+        return (char) randomNum;
+
+    }
+
+    private static int getRandomNumber(Random random, int min, int max) {
+
+        return random.nextInt((max - min) + 1) + min;
+
     }
 
     private static int getNumber(String prompt) {
